c015fd13f1f313753363b582ed52118f99497642
qpzm <lhm1442@gmail.com>
2020-02-23 04:16:38 +0900

    5. Create Franc by copying Dollar

--- money.rb
+++ money.rb
@@ -15,3 +15,21 @@ class Dollar
 
   attr_reader :amount
 end
+
+class Franc
+  def initialize(amount)
+    @amount = amount
+  end
+
+  def times(multiplier)
+    Franc.new(amount * multiplier)
+  end
+
+  def ==(other)
+    amount == other.amount
+  end
+
+  protected
+
+  attr_reader :amount
+end
--- money_test.rb
+++ money_test.rb
@@ -15,4 +15,14 @@ class DollarTest < Minitest::Test
 end
 
 class FrancTest < Minitest::Test
+  def test_multiplication
+    five = Franc.new 5
+    assert_equal Franc.new(10), five.times(2)
+    assert_equal Franc.new(15), five.times(3)
+  end
+
+  def test_equality
+    assert_equal Franc.new(5), Franc.new(5)
+    refute_equal Franc.new(5), Franc.new(6)
+  end
 end
