d01b8bceda539ee67176677104addbe2355e186b
qpzm <lhm1442@gmail.com>
2020-02-23 03:15:06 +0900

    2. Turn Dollar into a value object

--- money_test.rb
+++ money_test.rb
@@ -4,7 +4,12 @@ require_relative 'money'
 class MoneyTest < Minitest::Test
   def test_multiplication
     five = Dollar.new 5
+    product = five.times 2
     five.times 2
-    assert_equal 10, five.amount
+    assert_equal 10, product.amount
+
+    product = five.times 3
+    five.times 3
+    assert_equal 15, product.amount
   end
 end
