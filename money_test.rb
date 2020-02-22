3a7f4819bbd56c793b4624724f7ab694126360e5
qpzm <lhm1442@gmail.com>
2020-02-23 05:16:29 +0900

    11. Delete unnecessary tests

--- money_test.rb
+++ money_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative 'money'
 
-class DollarTest < Minitest::Test
+class MoneyTest < Minitest::Test
   def test_multiplication
     five = Money.dollar 5
     assert_equal Money.dollar(10), five.times(2)
@@ -11,30 +11,11 @@ class DollarTest < Minitest::Test
   def test_equality
     assert_equal Money.dollar(5), Money.dollar(5)
     refute_equal Money.dollar(5), Money.dollar(6)
-  end
-
-  def test_currency
-    assert_equal "USD", Money.dollar(1).currency
-  end
-end
-
-class FrancTest < Minitest::Test
-  def test_multiplication
-    five = Money.franc 5
-    assert_equal Money.franc(10), five.times(2)
-    assert_equal Money.franc(15), five.times(3)
-  end
-
-  def test_equality
-    assert_equal Money.franc(5), Money.franc(5)
-    refute_equal Money.franc(5), Money.franc(6)
-  end
-
-  def test_equality_with_dollar
     refute_equal Money.franc(5), Money.dollar(5)
   end
 
   def test_currency
+    assert_equal "USD", Money.dollar(1).currency
     assert_equal "CHF", Money.franc(1).currency
   end
 end
