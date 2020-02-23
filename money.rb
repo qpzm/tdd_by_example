d7a79849286844ee6d73203404963fdfbdb617ca
qpzm <lhm1442@gmail.com>
2020-02-23 17:01:48 +0900

    14.2. Delegate a bank to calculate exchange rate

--- money.rb
+++ money.rb
@@ -27,8 +27,8 @@ class Money
     Sum.new(self, other)
   end
 
-  def reduce(to)
-    rate = currency == 'CHF' && to == 'USD' ? 2 : 1
+  def reduce(bank, to)
+    rate = bank.rate(currency, to)
     Money.new(amount / rate, to)
   end
 end
@@ -39,10 +39,14 @@ class Bank
   end
 
   def reduce(expr, to)
-    expr.reduce(to)
+    expr.reduce(self, to)
   end
 
   def add_rate(from, to, rate)
+  end
+
+  def rate(from, to)
+    from == 'CHF' && to == 'USD' ? 2 : 1
   end
 end
 
@@ -54,7 +58,7 @@ class Sum
     @addend = addend
   end
 
-  def reduce(to)
+  def reduce(bank, to)
     amount = augend.amount + addend.amount
     Money.new(amount, to)
   end
